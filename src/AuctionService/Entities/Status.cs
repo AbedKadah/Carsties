0a53c81ff943840f5f0b1522d9ace08432a27d15
AK <abedallh.kadah@gmail.com>
2024-04-29 14:45:01 +0300

    End of Section 2

---
+++ src/AuctionService/Entities/Status.cs
@@ -0,0 +1,8 @@
+﻿namespace AcutionService.Entities;
+
+public enum Status
+{
+    Live,
+    Finished,
+    ReserveNotMet
+}
---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AcutionService.DTOs;
+using AcutionService.Entities;
+using AutoMapper;
+
+namespace AcutionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>().ForMember(d => d.Item, o => o.MapFrom(s => s));
+        CreateMap<CreateAuctionDto, Item>();
+
+    }
+}
